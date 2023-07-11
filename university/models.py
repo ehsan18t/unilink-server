d296dd030dd2a3b9ad1b1c131757cfab0878c3db
Syed Eftasum Alam <62606974+Syed-Eftasum-Alam@users.noreply.github.com>
2023-07-12 00:22:01 +0600

    added model for university settings

--- university/models.py
+++ university/models.py
@@ -2,9 +2,16 @@ from django.db import models
 from users.models import UserAccount
 
 
+class Settings(models.Model):
+    email_pattern = models.CharField(max_length=100)  # university pattern
+    sid_pattern = models.CharField(max_length=100)  # student id pattern
+    is_registration_enabled = models.BooleanField(default=False)
+
+
 class University(models.Model):
     name = models.CharField(max_length=100)
     domain = models.CharField(max_length=300)
+    settings = models.ForeignKey(Settings, on_delete=models.CASCADE)
     admin = models.ForeignKey(UserAccount, on_delete=models.CASCADE)
 
     def __str__(self):
