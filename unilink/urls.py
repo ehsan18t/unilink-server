47311d35c002babcfb48c0ea7b30a315b242d5d4
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2023-07-12 00:09:03 +0600

    added university app

--- unilink/urls.py
+++ unilink/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('djoser.urls')),
     path('api/', include('users.urls')),
+    path('api/university/', include('university.urls')),
 ]
