9bc47b58ebf50f36bd6bf84cbc224af28a115bfd
Syed Eftasum Alam <62606974+Syed-Eftasum-Alam@users.noreply.github.com>
2023-07-12 00:21:16 +0600

    added university models

--- university/models.py
+++ university/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class University(models.Model):
+    name = models.CharField(max_length=100)
+    domain = models.CharField(max_length=300)
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Department(models.Model):
+    name = models.CharField(max_length=100)
+    code = models.CharField(max_length=100)
+    university = models.ForeignKey(University, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.name}'
