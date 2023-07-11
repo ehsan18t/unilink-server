06ddbe9d0facb1487ffe5e8bde66b21361ea36df
Syed Eftasum Alam <62606974+Syed-Eftasum-Alam@users.noreply.github.com>
2023-07-12 00:21:41 +0600

    added university admin

--- university/models.py
+++ university/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from users.models import UserAccount
 
 
 class University(models.Model):
     name = models.CharField(max_length=100)
     domain = models.CharField(max_length=300)
+    admin = models.ForeignKey(UserAccount, on_delete=models.CASCADE)
 
     def __str__(self):
         return f'{self.name}'
