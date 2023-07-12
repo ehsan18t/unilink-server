69ddb5d18c7313c3960ebcbe6ef3a6caa4af380d
mdashik313 <mdashik5360@gmail.com>
2023-07-12 08:44:53 +0600

    approved university list api

--- university/urls.py
+++ university/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import create_university, pending_university_list
+from .views import create_university, pending_university_list, approved_university_list
 
 urlpatterns = [
     path('create/', create_university, name='create-university'),
     path('pending/', pending_university_list, name='pending-university-list'),
+    path('approved/', approved_university_list, name='approved-university-list'),
 ]
