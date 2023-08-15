c42acc20b34e6487d640c8112a82f95da8d2e657
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2023-08-15 18:23:00 +0600

    added id to user serializer

--- users/serializers.py
+++ users/serializers.py
@@ -19,4 +19,4 @@ class CustomUserCreateSerializerRetype(CustomUserCreateSerializer):
 class UserAccountSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserAccount
-        fields = ['first_name', 'last_name', 'username', 'email', 'university', 'department', 'profile_picture', 'user_type']
+        fields = ['id', 'first_name', 'last_name', 'username', 'email', 'university', 'department', 'profile_picture', 'user_type']
