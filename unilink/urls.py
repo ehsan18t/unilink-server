d4b9683313a61dbb4e94e83454df3b8609184bb6
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2023-08-15 15:56:03 +0600

    allow media for public access

--- unilink/urls.py
+++ unilink/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -7,4 +9,4 @@ urlpatterns = [
     path('api/', include('users.urls')),
     path('api/university/', include('university.urls')),
     path('api/department/', include('department.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
