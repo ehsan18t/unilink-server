2a0272359321b17a14eca9017cfe1bc53543bf20
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2023-08-15 19:25:02 +0600

    added api endpoint to update university approve and ban status

--- university/urls.py
+++ university/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from .views import create_university, pending_university_list, approved_university_list, approved_university_list_public
+from .views import *
 
 urlpatterns = [
     path('create/', create_university, name='create-university'),
     path('pending/', pending_university_list, name='pending-university-list'),
     path('approved/', approved_university_list, name='approved-university-list'),
     path('list/', approved_university_list_public, name='approved-university-list-public'),
+    path('approve/', approve_university, name='approve-university'),
+    path('disapprove/', disapprove_university, name='disapprove-university'),
+    path('ban/', ban_university, name='ban-university'),
+    path('unban/', unban_university, name='unban-university'),
 ]
