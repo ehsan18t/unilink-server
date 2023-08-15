ff292db66ccc6869d119ad7c0f72ba5a22f686b0
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2023-08-15 22:05:19 +0600

    added necessary permission classes

--- users/permissions.py
+++ users/permissions.py
@@ -5,3 +5,21 @@ class SiteAdminOnly(BasePermission):
     def has_permission(self, request, view):
         # Check if the user has the required type
         return request.user.is_authenticated and request.user.user_type == 0
+
+
+class UniversityAdminOnly(BasePermission):
+    def has_permission(self, request, view):
+        # Check if the user has the required type
+        return request.user.is_authenticated and request.user.user_type == 1
+
+
+class FacultyRepresentative(BasePermission):
+    def has_permission(self, request, view):
+        # Check if the user has the required type
+        return request.user.is_authenticated and request.user.user_type == 3 or request.user.user_type == 4
+
+
+class FacultyToStudent(BasePermission):
+    def has_permission(self, request, view):
+        # Check if the user has the required type
+        return request.user.is_authenticated and request.user.user_type > 2
