f2cad5f42448ad3476200b35fe05dbe718c4db41
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2023-08-15 22:49:40 +0600

    fixed forum post fetch failed

--- forum/views.py
+++ forum/views.py
@@ -1,14 +1,12 @@
-from django.shortcuts import render
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
-
 from .serializers import *
 from users.permissions import *
 from rest_framework.response import Response
 
 
 @api_view(['GET'])
-@permission_classes([AllowAny])
+@permission_classes([AdminToStudent])
 def forum_list(request):
     # get university id from requested users
     university = request.user.university
@@ -32,7 +30,7 @@ def forum_category_list(request):
 @api_view(['GET'])
 @permission_classes([AdminToStudent])
 def forum_post_list(request):
-    forum_id = request.GET.get('forum_id')
+    forum_id = request.data.get('forum_id')
     forum = Forum.objects.get(id=forum_id)
 
     # Get the list of approved forums
