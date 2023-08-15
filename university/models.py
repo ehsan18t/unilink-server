9adf75d6bd00ef268fc793dfee5ee8311de2ae2c
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2023-08-15 18:43:55 +0600

    updated university model with is_banned status

--- university/models.py
+++ university/models.py
@@ -14,6 +14,7 @@ class University(models.Model):
     settings = models.ForeignKey(Settings, on_delete=models.DO_NOTHING, null=True, blank=True)
     admin = models.ForeignKey(UserAccount, on_delete=models.DO_NOTHING, related_name='university_admin')
     is_approved = models.BooleanField(default=False)
+    is_banned = models.BooleanField(default=False)
 
     def __str__(self):
         return f'{self.name}'
