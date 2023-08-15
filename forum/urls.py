ce590a795964e0c473b941730d3e42a3c5108011
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2023-08-15 22:32:26 +0600

    added some get endpoint for forum api

--- forum/urls.py
+++ forum/urls.py
@@ -2,4 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
+    path('forum-list/', forum_list, name='forum-list'),
+    path('forum-category-list/', forum_category_list, name='forum-category-list'),
+    path('forum-post-list/', forum_post_list, name='forum-post-list'),
 ]
--- forum/views.py
+++ forum/views.py
@@ -1,3 +1,42 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+from .serializers import *
+from users.permissions import *
+from rest_framework.response import Response
+
+
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def forum_list(request):
+    # get university id from requested users
+    university = request.user.university
+
+    forums = Forum.objects.filter(university=university)
+    serializer = ForumSerializer(forums, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+@permission_classes([AdminToStudent])
+def forum_category_list(request):
+    # Get the list of approved forums
+    university = request.user.university
+
+    forums = ForumCategory.objects.filter(university=university)
+    serializer = ForumCategorySerializer(forums, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+@permission_classes([AdminToStudent])
+def forum_post_list(request):
+    forum_id = request.GET.get('forum_id')
+    forum = Forum.objects.get(id=forum_id)
+
+    # Get the list of approved forums
+    forums = ForumPost.objects.filter(forum=forum)
+    serializer = ForumPostSerializer(forums, many=True)
+    return Response(serializer.data)
+
