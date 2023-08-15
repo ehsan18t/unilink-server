e0adbb9ffd4146de73b8ce79f7a2d75d1f4d36e9
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2023-08-15 21:55:56 +0600

    added basic forum serializers

---
+++ forum/serializers.py
@@ -0,0 +1,39 @@
+from rest_framework import serializers
+from .models import *
+
+
+class ForumAdminSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ForumAdmin
+        fields = '__all__'
+
+
+class ForumCategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ForumCategory
+        fields = '__all__'
+
+
+class ForumSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Forum
+        fields = '__all__'
+
+
+class ForumPostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ForumPost
+        fields = '__all__'
+
+
+class ForumPostCommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ForumPostComment
+        fields = '__all__'
+
+
+class ForumPostLikeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ForumPostLike
+        fields = '__all__'
+
