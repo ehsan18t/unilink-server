c3f331788a470eb2c181b38c406721ec7e458762
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2023-08-15 22:49:12 +0600

    add permission for site admin for debug purpose

--- users/permissions.py
+++ users/permissions.py
@@ -34,4 +34,4 @@ class StudentOnly(BasePermission):
 class AdminToStudent(BasePermission):
     def has_permission(self, request, view):
         # Check if the user has the required type
-        return request.user.is_authenticated and request.user.user_type > 0
+        return request.user.is_authenticated and request.user.user_type > -1
