f83b851ec03fd67a31863e92cf0f5072e89e59e2
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2023-08-16 21:02:58 +0600

    added home page for server

--- unilink/urls.py
+++ unilink/urls.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import index
 
 urlpatterns = [
+    path('', index, name='index'),
     path('admin/', admin.site.urls),
     path('api/', include('djoser.urls')),
     path('api/', include('users.urls')),
