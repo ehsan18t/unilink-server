23218220337f251badd32742a04e666be16bff62
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2023-08-21 18:00:56 +0600

    added course app with models

---
+++ course/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from enum import Enum
+
+
+class CourseType(Enum):
+    THEORY = '1'
+    LAB = '2'
+
+    @classmethod
+    def choices(cls):
+        return [(key.value, key.value) for key in cls]
+
+
+class Section(models.Model):
+    name = models.CharField(max_length=5)
+    trimester = models.CharField(max_length=50)
+    course = models.ForeignKey('Course', on_delete=models.CASCADE, related_name='sections')
+    students = models.ManyToManyField('users.UserAccount', related_name='sections')
+    faculty = models.ManyToManyField('users.UserAccount', related_name='sections_faculty')
+
+    def __str__(self):
+        return {self.name}
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=200)
+    code = models.CharField(max_length=50)
+    type = models.IntegerField(choices=CourseType.choices(), default=CourseType.THEORY.value)
+
+    def __str__(self):
+        return f'{self.code} {self.name}'
