b0697e289c723eadb4735fc8e8fdf84a48d4c184
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2023-08-22 08:08:58 +0600

    updated models

--- course/models.py
+++ course/models.py
@@ -11,21 +11,23 @@ class CourseType(Enum):
         return [(key.value, key.value) for key in cls]
 
 
+class Course(models.Model):
+    name = models.CharField(max_length=200)
+    code = models.CharField(max_length=50)
+    type = models.IntegerField(choices=CourseType.choices(), default=CourseType.THEORY.value)
+    university = models.ForeignKey('university.University', on_delete=models.CASCADE, related_name='university')
+
+    def __str__(self):
+        return f'{self.code} {self.name}'
+
+
 class Section(models.Model):
     name = models.CharField(max_length=5)
     trimester = models.CharField(max_length=50)
-    course = models.ForeignKey('Course', on_delete=models.CASCADE, related_name='sections')
-    students = models.ManyToManyField('users.UserAccount', related_name='sections')
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='course')
+    students = models.ManyToManyField('users.UserAccount', related_name='students')
     faculty = models.ManyToManyField('users.UserAccount', related_name='sections_faculty')
 
     def __str__(self):
         return f'{self.trimester} {self.name}'
 
-
-class Course(models.Model):
-    name = models.CharField(max_length=200)
-    code = models.CharField(max_length=50)
-    type = models.IntegerField(choices=CourseType.choices(), default=CourseType.THEORY.value)
-
-    def __str__(self):
-        return f'{self.code} {self.name}'
