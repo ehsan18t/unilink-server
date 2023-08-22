bdab498096b0f5a9fcd08747578e01e446b70737
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2023-08-22 11:03:27 +0600

    update course model

--- course/models.py
+++ course/models.py
@@ -14,6 +14,7 @@ class CourseType(Enum):
 class Course(models.Model):
     name = models.CharField(max_length=200)
     code = models.CharField(max_length=50)
+    department = models.ForeignKey('department.Department', on_delete=models.CASCADE, related_name='department')
     type = models.IntegerField(choices=CourseType.choices(), default=CourseType.THEORY.value)
     university = models.ForeignKey('university.University', on_delete=models.CASCADE, related_name='university')
 
